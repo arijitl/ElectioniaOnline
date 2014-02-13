311c6f83fe295d64dd7d994f62b1026920fe4618
Ptotem Learning Projects <info@ptotem.com>
2014-02-14 02:52:55 +0530

    Changed Time Zone

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module ElectioniaOnline
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Asia/Kolkata'
+    config.time_zone = 'Mumbai'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
