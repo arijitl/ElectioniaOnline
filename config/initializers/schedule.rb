55a4ebd57b34f4cc8b347f5636560ca850597ea2
Nilesh Panchal <neil08.panchal@gmail.com>
2014-02-28 16:59:58 +0530

    rakesh merged

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -2,13 +2,13 @@ require 'rubygems'
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
-scheduler.cron '31 16 * * *' do
+scheduler.cron '56 16 * * *' do
   puts 'Cron Started'
   Game.game_evaluate
   puts 'Cron ended'
 end
 
-scheduler.cron '31 16 * * *' do
+scheduler.cron '57 16 * * *' do
   puts 'Cron Started'
   WeeklyLeader.add_weekly_leader
   puts 'Cron ended'
