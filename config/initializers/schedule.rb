522c719f6cfd2f7d7abc4d37dee155acbd4b22b7
RakeshRengaraj <rakesh.rengaraj@gmail.com>
2014-02-28 15:56:14 +0530

    rufus scheduler

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -2,8 +2,9 @@ require 'rubygems'
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
-scheduler.cron '55 12 * * *' do
+scheduler.cron '55 15 * * *' do
   puts 'Cron Started'
+  Game.game_evaluate
   puts 'Cron ended'
 end
 
