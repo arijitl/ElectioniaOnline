d82af7ee7386c767764c69765d9335441a15191d
Sunny <sunny.220990@gmail.com>
2014-03-01 11:50:54 +0530

    Cron job timing is in line with the server timing

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -2,13 +2,13 @@ require 'rubygems'
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
-scheduler.cron '56 16 * * *' do
+scheduler.cron '25 18 * * *' do
   puts 'Cron Started'
   Game.game_evaluate
   puts 'Cron ended'
 end
 
-scheduler.cron '57 16 * * *' do
+scheduler.cron '26 18 * * *' do
   puts 'Cron Started'
   WeeklyLeader.add_weekly_leader
   puts 'Cron ended'
