3cf6147a0c42d0623d0c2eb8cb9317ac2687d365
NileshPanchal <neil08.panchal@gmail.com>
2014-03-05 16:28:26 +0530

    leader board balance check

--- app/models/weekly_leader.rb
+++ app/models/weekly_leader.rb
@@ -15,8 +15,7 @@ class WeeklyLeader < ActiveRecord::Base
         @user_res<<GameResult.where('game_id== ? AND user_id= ?',q.id,u).last
       end
       @user_res=@user_res.delete_if { |x| x==nil }
-      #@week_leader<<{:user_id => u, :score => @user_res.map { |i| (i.income+(1000-i.expense)) rescue 0 }.delete_if{|x| x==nil}.sum}
-      @week_leader<<{:user_id => u, :score => @user_res.map { |i| i.income rescue 0 }.delete_if{|x| x==nil}.sum}
+      @week_leader<<{:user_id => u, :score => @user_res.map { |i| (i.income+(User.find(u).bank-i.income)) rescue 0 }.delete_if{|x| x==nil}.sum}
     end
 
     @week_leader.sort_by {|hash| hash[:score]}.reverse[0..5].each do |winner|
